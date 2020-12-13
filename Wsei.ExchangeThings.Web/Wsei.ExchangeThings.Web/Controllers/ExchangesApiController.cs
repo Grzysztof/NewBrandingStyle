4b7918c4f1a1abefe5ca0dd37f76d2dd2adcdd82
kmyjak <kmyjak@wsei.edu.pl>
2020-12-13 07:30:56 +0100

    lab3-db

---
+++ Wsei.ExchangeThings.Web/Wsei.ExchangeThings.Web/Controllers/ExchangesApiController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Wsei.ExchangeThings.Web.Models;
+using Wsei.ExchangeThings.Web.Database;
+using Wsei.ExchangeThings.Web.Entities;
+
+namespace Wsei.ExchangeThings.Web.Controllers
+{
+    [ApiController]
+    [Route("api/exchanges")]
+    public class ExchangesApiController : ControllerBase
+    {
+        private ExchangesDbContext _dbContext;
+
+        public ExchangesApiController(ExchangesDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public AddNewItemResponse Post(ItemModel item)
+        {
+            var entity = new ItemEntity
+            {
+                Name = item.Name,
+                Description = item.Description,
+                IsVisible = item.IsVisible,
+            };
+
+            _dbContext.Items.Add(entity);
+            _dbContext.SaveChanges();
+
+            var response = new AddNewItemResponse()
+            {
+                Success = true,
+            };
+
+
+            return response;
+        }
+    }
+}
--- Wsei.ExchangeThings.Web/Wsei.ExchangeThings.Web/Startup.cs
+++ Wsei.ExchangeThings.Web/Wsei.ExchangeThings.Web/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace Wsei.ExchangeThings.Web
 {
@@ -23,6 +24,7 @@ namespace Wsei.ExchangeThings.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<Database.ExchangesDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ExchangeThings")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
